e815ad636557162dad3011b0bbfffb48b5312ae7
collinewait <collinewait17@gmail.com>
2019-11-14 14:49:13 +0300

    add concurrency

--- scraper/scraper.go
+++ scraper/scraper.go
@@ -16,44 +16,27 @@ func Scrape(service *gmail.Service) {
 	var email = flag.String("email", "", "we are to query mesages against this email")
 
 	flag.Parse()
+	start := time.Now()
+	messagesChannel := make(chan *gmail.Message)
+	messageContentChannel := make(chan *gmail.Message)
+	attachmentChannel := make(chan *attachment)
+	doneChannel := make(chan bool)
 
-	messages := getMessages(service, email)
+	go getMessages(service, email, messagesChannel)
+	go getMessageContent(messagesChannel, messageContentChannel, service)
+	go getAttachment(service, messageContentChannel, attachmentChannel)
+	go saveAttachment(attachmentChannel, doneChannel)
 
-	for _, msg := range messages {
-		msgContent, _ := getMessageContent(msg.Id, service)
-		tm := time.Unix(0, msgContent.InternalDate*1e6)
-		for _, part := range msgContent.Payload.Parts {
-
-			if len(part.Filename) != 0 {
-				newFileName := tm.Format("Jan-02-2006") + "-" + part.Filename
-				attachment, _ := getAttachment(service, msgContent.Id, part.Body.AttachmentId)
-
-				decoded, _ := base64.URLEncoding.DecodeString(attachment.Data)
-				const path = "./attachments/"
-				if _, err := os.Stat(path); os.IsNotExist(err) {
-					os.Mkdir(path, os.ModePerm)
-				}
-				f, err := os.Create(path + newFileName)
-				if err != nil {
-					panic(err)
-				}
-				defer f.Close()
-
-				if _, err := f.Write(decoded); err != nil {
-					panic(err)
-				}
-				if err := f.Sync(); err != nil {
-					panic(err)
-				}
-
-			}
-
-		}
-	}
-
+	<-doneChannel
+	fmt.Println("Elapsed: ", time.Since(start))
 }
 
-func getMessages(service *gmail.Service, email *string) []*gmail.Message {
+type attachment struct {
+	data     string
+	fileName string
+}
+
+func getMessages(service *gmail.Service, email *string, msgsCh chan *gmail.Message) {
 	query := fmt.Sprintf("from:%s", *email)
 
 	msgs := []*gmail.Message{}
@@ -73,14 +56,62 @@ func getMessages(service *gmail.Service, email *string) []*gmail.Message {
 		fmt.Println("No messages found.")
 	}
 
-	return msgs
+	for _, msg := range msgs {
+		msgsCh <- msg
+	}
+	close(msgsCh)
 }
 
-func getMessageContent(messageID string, service *gmail.Service) (*gmail.Message, error) {
-	msg, err := service.Users.Messages.Get("me", messageID).Do()
-	return msg, err
+func getMessageContent(msgsCh, msgCh chan *gmail.Message, service *gmail.Service) {
+	for msg := range msgsCh {
+		msgContent, err := service.Users.Messages.Get("me", msg.Id).Do()
+		if err != nil {
+			log.Fatalf("Unable to retrieve Message Contents: %v", err)
+		}
+		msgCh <- msgContent
+	}
+	close(msgCh)
 }
 
-func getAttachment(service *gmail.Service, messageID string, attachmentID string) (*gmail.MessagePartBody, error) {
-	return service.Users.Messages.Attachments.Get("me", messageID, attachmentID).Do()
+func getAttachment(service *gmail.Service, msgContentCh chan *gmail.Message, attachCh chan *attachment) {
+	for msgContent := range msgContentCh {
+		attach := new(attachment)
+		tm := time.Unix(0, msgContent.InternalDate*1e6)
+		for _, part := range msgContent.Payload.Parts {
+			if len(part.Filename) != 0 {
+				newFileName := tm.Format("Jan-02-2006") + "-" + part.Filename
+				msgPartBody, err := service.Users.Messages.Attachments.Get("me", msgContent.Id, part.Body.AttachmentId).Do()
+				if err != nil {
+					log.Fatalf("Unable to retrieve Attachment: %v", err)
+				}
+				attach.data = msgPartBody.Data
+				attach.fileName = newFileName
+				attachCh <- attach
+			}
+		}
+	}
+	close(attachCh)
+}
+
+func saveAttachment(attachCh chan *attachment, doneCh chan bool) {
+	for attach := range attachCh {
+		decoded, _ := base64.URLEncoding.DecodeString(attach.data)
+		const path = "./attachments/"
+		if _, err := os.Stat(path); os.IsNotExist(err) {
+			os.Mkdir(path, os.ModePerm)
+		}
+		f, err := os.Create(path + attach.fileName)
+		if err != nil {
+			panic(err)
+		}
+		defer f.Close()
+
+		if _, err := f.Write(decoded); err != nil {
+			panic(err)
+		}
+		if err := f.Sync(); err != nil {
+			panic(err)
+		}
+	}
+	doneCh <- true
 }
